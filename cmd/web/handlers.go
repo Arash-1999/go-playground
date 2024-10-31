76fdc6a275f9a7416ab801409804826b5075a0ba
arash-shafiee <i.arash.shafiee@gmail.com>
2024-10-31 18:02:31 +0330

    feat: postgres

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 func (app *Application) homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -28,16 +30,57 @@ func (app *Application) postSnippet(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("something created"))
 }
 
+type Snippet struct {
+	ID      int
+	Title   string
+	Content string
+	Created time.Time
+	Expires time.Time
+}
+
 func (app *Application) getSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Has("id") {
+		id, err := strconv.Atoi(r.URL.Query().Get("id"))
 
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+		if err != nil {
+			http.NotFound(w, r)
+			app.logger.Error("Not Found", "path", r.URL.Path, "snippet_id", id)
+			return
+		}
+		app.logger.Info("get snippet", "id", id)
+		fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	} else {
+		stmt := `SELECT id, title, content, created, expires FROM snippets
+    WHERE expires > now() ORDER BY id DESC LIMIT 10`
 
-	if err != nil {
-		http.NotFound(w, r)
-		app.logger.Error("Not Found", "path", r.URL.Path, "snippet_id", id)
-		return
+		rows, err := app.db.Query(context.Background(), stmt)
+
+		if err != nil {
+			app.logger.Error("Query Failed", "error", err.Error())
+		}
+
+		defer rows.Close()
+
+		snippets := []*Snippet{}
+
+		for rows.Next() {
+			s := &Snippet{}
+
+			err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
+			if err != nil {
+				app.logger.Error("snippets", "error", err.Error())
+			}
+
+			snippets = append(snippets, s)
+		}
+
+		if err = rows.Err(); err != nil {
+			app.logger.Error("snippets", "error", err.Error())
+		}
+
+		for _, snippet := range snippets {
+			fmt.Fprintf(w, "%+v\n", snippet)
+		}
 	}
-
-	app.logger.Info("get snippet", "id", id)
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 
 	"lets-go-book-2022/internal/config"
 )
 
 type Application struct {
 	logger slog.Logger
+	db     *pgxpool.Pool
 }
 
 var configPath = flag.String("config", "internal/config/config.yaml", "yaml config path")
@@ -19,8 +23,15 @@ func main() {
 	// TODO: use signal to update flags
 	config.Load(*configPath)
 
+	db, err := initDb(config.Configs.DB.DSN)
+	logger := *config.ConfigLogger()
+
+	if err != nil {
+		logger.Error("Database Connection Error", "error", err)
+	}
 	app := &Application{
-		logger: *config.ConfigLogger(),
+		logger: logger,
+		db:     db,
 	}
 
 	// TODO: move routes to a new file (routes.go, ...)
@@ -39,6 +50,17 @@ func main() {
 	}
 
 	app.logger.Info("Starting server", "port", config.Configs.Server.Port)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	app.logger.Error(err.Error())
+}
+
+func initDb(dsn string) (*pgxpool.Pool, error) {
+	// db, err := sql.Open("postgres", dsn)
+	db, err := pgxpool.New(context.Background(), dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
