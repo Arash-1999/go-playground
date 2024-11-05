d47982226a6ef42374b755ea4176f8a2d3083c0b
arash-shafiee <i.arash.shafiee@gmail.com>
2024-11-05 18:06:29 +0330

    fix: export clinet error in global errors

--- cmd/web/base/error.go
+++ cmd/web/base/error.go
@@ -11,10 +11,10 @@ func (app *Application) ServerError(w http.ResponseWriter, err error) {
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
-func (app *Application) clientError(w http.ResponseWriter, status int) {
+func (app *Application) ClientError(w http.ResponseWriter, status int) {
 	http.Error(w, http.StatusText(status), status)
 }
 
 func (app *Application) NotFound(w http.ResponseWriter) {
-	app.clientError(w, http.StatusNotFound)
+	app.ClientError(w, http.StatusNotFound)
 }
